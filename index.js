1fd667c96370a5c5c09e3d4127f83bd3f1481e40
Aqib-Ahmad <aqibkhan0396@gmail.com>
2026-08-22 17:13:13 +0530

    adding updated code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const helmet = require('helmet');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -11,21 +11,31 @@ connectDB();
 
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require("./routes/apiRoutes");
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://triraxtech.com",
+      "https://www.triraxtech.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.get('/', (req, res) => {
-    res.send('TrirexTech API is running...');
+app.get("/", (req, res) => {
+  res.send("TrirexTech API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
